8aa1aa77cc1779b9be55d3ca5fb02a02294fafa8
Hidaayat <hidaayat07@gmail.com>
2023-05-31 10:37:35 +0200

    initial commit

---
+++ bus-travel.js
@@ -0,0 +1,31 @@
+function Bus Travel() {
+
+    // object that stores the destination and fares
+    const destinationFares = {
+        "Khayelitsha": 40,
+        "Dunoon": 25,
+        "Mitchell's Plain": 30
+
+    }
+
+    let costSingle = 0;
+    let numOfTripsMake = 0;
+    //function that calculates the cost trip for single using two parameters points and location
+    function costPerTripSingle(points, location) {
+
+    }
+    //function that gets the cost per single trip
+    function getCostSingleTrip() {
+        return costSingle;
+    }
+
+    //function that calculates the number of trips that can be made using
+    //two parameters points and location
+    function numOfTrips(points, location) {
+
+    }
+    //function that gets the number of trips that can be made
+    function getNumOfTrips() {
+        return numOfTripsMake;
+    }
+}
